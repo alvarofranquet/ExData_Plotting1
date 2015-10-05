4dc571810c8c1f9a49d169335039764c65cb7371
alvarofranquet <alvarofranquet@hotmail.com>
2015-10-05 21:06:58 +0200

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,15 @@
+# 0. Read the data.
+
+require(data.table)
+
+md <-read.csv("household_power_consumption.txt", sep=";", stringsAsFactors=FALSE)
+md <- tidyr::separate(md, Date, into = c('day','month', 'year'), sep = '/')
+md <- dplyr::mutate(md, Date = paste(year,month, day, sep = '-'))
+md <- dplyr::mutate(md, Date = lubridate::ymd_hms(paste(Date, Time, sep = ' ')))
+
+md <- dplyr::filter(md, lubridate::year(Date) == 2007 &  lubridate::month(Date) == 2 & lubridate::day(Date) %in% c(1,2))
+
+# Plot 1
+png(file = "plot1.png", bg = "transparent")
+hist(as.numeric(md$Global_active_power), col = 'red', main = 'Global Active Power')
+dev.off()
